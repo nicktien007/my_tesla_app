1cf76caec21266cd75020b2ce6b3a126e0349f9a
nick <nick.tian@guisitech.com>
2025-07-25 21:01:49 +0800

    調整 ChargedLogEntry 結構，將 id 屬性改為可選，並更新初始化方法以確保正確解碼。

--- app/my-tesla-app/my-tesla-app/ChargedLogEntry.swift
+++ app/my-tesla-app/my-tesla-app/ChargedLogEntry.swift
@@ -1,7 +1,8 @@
 import Foundation
 
+
 struct ChargedLogEntry: Identifiable, Codable {
-    let id = UUID()
+    let id: UUID
     let date: String
     let totalMileage: String?
     let stageMileage: String?
@@ -11,4 +12,31 @@ struct ChargedLogEntry: Identifiable, Codable {
     let totalCost: String?
     let chargeType: String?
     let note: String?
+
+    init(id: UUID = UUID(), date: String, totalMileage: String?, stageMileage: String?, chargedKWh: String?, efficiency: String?, pricePerKWh: String?, totalCost: String?, chargeType: String?, note: String?) {
+        self.id = id
+        self.date = date
+        self.totalMileage = totalMileage
+        self.stageMileage = stageMileage
+        self.chargedKWh = chargedKWh
+        self.efficiency = efficiency
+        self.pricePerKWh = pricePerKWh
+        self.totalCost = totalCost
+        self.chargeType = chargeType
+        self.note = note
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = (try? container.decode(UUID.self, forKey: .id)) ?? UUID()
+        self.date = try container.decode(String.self, forKey: .date)
+        self.totalMileage = try container.decodeIfPresent(String.self, forKey: .totalMileage)
+        self.stageMileage = try container.decodeIfPresent(String.self, forKey: .stageMileage)
+        self.chargedKWh = try container.decodeIfPresent(String.self, forKey: .chargedKWh)
+        self.efficiency = try container.decodeIfPresent(String.self, forKey: .efficiency)
+        self.pricePerKWh = try container.decodeIfPresent(String.self, forKey: .pricePerKWh)
+        self.totalCost = try container.decodeIfPresent(String.self, forKey: .totalCost)
+        self.chargeType = try container.decodeIfPresent(String.self, forKey: .chargeType)
+        self.note = try container.decodeIfPresent(String.self, forKey: .note)
+    }
 }
